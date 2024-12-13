25dfcab3627368a1bffbe8c4caf95c956ef72492
Ahenrie <10914763@uvu.edu>
2024-12-12 19:06:49 -0800

    holy shit it worked

--- day1/main.go
+++ day1/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -16,4 +20,31 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
+	first_column := []int{}
+	second_column := []int{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Fields(line)
+
+		if num, err := strconv.Atoi(parts[0]); err == nil {
+			first_column = append(first_column, num)
+		}
+		if num, err := strconv.Atoi(parts[1]); err == nil {
+			second_column = append(second_column, num)
+		}
+	}
+
+	sort.Ints(first_column)
+	sort.Ints(second_column)
+	fmt.Println(first_column)
+	fmt.Println(second_column)
+
+	var total int
+	for i, _ := range first_column {
+		diff := first_column[i] - second_column[i]
+		total += int(math.Abs(float64(diff)))
+
+	}
+	fmt.Println(total)
 }

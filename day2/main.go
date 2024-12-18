5ef4a75406f0a246509a283dddbc16c960961470
Ahenrie <10914763@uvu.edu>
2024-12-18 11:26:41 -0800

    Close

--- day2/main.go
+++ day2/main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func printLine(inScanner *bufio.Scanner) {
+	for inScanner.Scan() {
+		fmt.Println(inScanner.Text())
+	}
+}
+
+func splitScannerRows(inScanner *bufio.Scanner) [][]int {
+	slices := [][]int{}
+	for inScanner.Scan() {
+		buffer := []int{}
+		line := inScanner.Text()
+		// Split the line into parts by whitespace
+		parts := strings.Fields(line)
+		for _, part := range parts {
+			if num, err := strconv.Atoi(part); err == nil {
+				//fmt.Println(num)
+				buffer = append(buffer, num)
+			} else {
+				fmt.Println("Error converting string to int:", err)
+			}
+		}
+		slices = append(slices, buffer)
+	}
+	return slices
+}
+
+func safeOrUnsafe(levels [][]int) (int, int) {
+	for _, level := range levels {
+
+	}
+	return 0, 1
+}
 
 func main() {
-	fmt.Println("hey")
+	var safe int
+	var unsafe int
+
+	// Read in file
+	file, err := os.Open("puzzle.txt")
+	if err != nil {
+		fmt.Println("Error occured on file read:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	levels := splitScannerRows(scanner)
+	safe, unsafe = safeOrUnsafe(levels)
+	println(safe, unsafe)
 }

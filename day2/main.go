2b1ed5ad37107af90c59ae14d48bf967324b3058
Ahenrie <10914763@uvu.edu>
2024-12-21 20:13:16 -0800

    Day two done late lol

--- day2/main.go
+++ day2/main.go
@@ -34,18 +34,66 @@ func splitScannerRows(inScanner *bufio.Scanner) [][]int {
 	return slices
 }
 
-// need logic
-func safeOrUnsafe(levels [][]int) (int, int) {
-	for _, level := range levels {
-
+// increasing or decreasing
+func increasingOrDecreasing(slice []int) string {
+	if len(slice) < 2 {
+		return "neither"
 	}
-	return 0, 1
+
+	increasing := true
+	decreasing := true
+
+	for i := 1; i < len(slice); i++ {
+		if slice[i] > slice[i-1] {
+			decreasing = false
+		}
+		if slice[i] < slice[i-1] {
+			increasing = false
+		}
+	}
+
+	if increasing {
+		return "increasing"
+	}
+	if decreasing {
+		return "decreasing"
+	}
+	return "neither"
+}
+
+// adjacent checks if the difference between adjacent numbers in the slice is 1, 2, or 3.
+func adjacent(slice []int) bool {
+	// I do not want to convert to float64
+	abs := func(x int) int {
+		if x < 0 {
+			return -x
+		}
+		return x
+	}
+
+	for i := 1; i < len(slice); i++ {
+		diff := abs(slice[i] - slice[i-1])
+		if diff != 1 && diff != 2 && diff != 3 {
+			return false
+		}
+	}
+	return true
+}
+
+func safeOrUnsafe(levels [][]int) (int, int) {
+	safe := 0
+	unsafe := 0
+	for _, level := range levels {
+		if increasingOrDecreasing(level) != "neither" && adjacent(level) == true {
+			safe = safe + 1
+		} else {
+			unsafe = unsafe + 1
+		}
+	}
+	return safe, unsafe
 }
 
 func main() {
-	var safe int
-	var unsafe int
-
 	// Read in file
 	file, err := os.Open("puzzle.txt")
 	if err != nil {
@@ -56,6 +104,6 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	levels := splitScannerRows(scanner)
-	safe, unsafe = safeOrUnsafe(levels)
-	println(safe, unsafe)
+	safe, unsafe := safeOrUnsafe(levels)
+	fmt.Printf("Safe: %v, Unsafe: %v:", safe, unsafe)
 }
